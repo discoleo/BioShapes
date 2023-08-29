0780fde91ecc827582da44a3f037a4b2dcab5159
discoleo <leo.mada@syonic.eu>
2023-08-29 17:37:55 +0300

    Tests: Circle through 3 points;

--- tests/experimental/Tests.Circles.R
+++ tests/experimental/Tests.Circles.R
@@ -4,6 +4,62 @@
 
 ### Circles
 
+
+### Through 3 Points
+
+
+# VH
+x = c(1,1,5); y = c(1,4,1)
+cc = circle.p3(x, y)
+plot.base()
+lines(as.bioshape(list(cc)))
+polygon(x, y, border="blue")
+points(x, y, col="red")
+
+# V-Narrow
+x = c(1,1,5); y = c(1,4,3)
+cc = circle.p3(x, y)
+plot.base()
+lines(as.bioshape(list(cc)))
+polygon(x, y, border="blue")
+points(x, y, col="red")
+
+# V-Obtuse
+x = c(1,1,5); y = c(1,4,0)
+cc = circle.p3(x, y)
+plot.base()
+lines(as.bioshape(list(cc)))
+polygon(x, y, border="blue")
+points(x, y, col="red")
+
+# H-Narrow
+x = c(1,5,4); y = c(1,1,8)
+cc = circle.p3(x, y)
+plot.base()
+lines(as.bioshape(list(cc)))
+polygon(x, y, border="blue")
+points(x, y, col="red")
+
+# H-Obtuse
+x = c(1,8,6); y = c(1,1,0)
+cc = circle.p3(x, y)
+plot.base()
+lines(as.bioshape(list(cc)))
+polygon(x, y, border="blue")
+points(x, y, col="red")
+
+# G-Obtuse
+x = c(1,8,6); y = c(1,2,0)
+cc = circle.p3(x, y)
+plot.base()
+lines(as.bioshape(list(cc)))
+polygon(x, y, border="blue")
+points(x, y, col="red")
+
+
+##############################
+##############################
+
 ### Tangent & through 2 Points
 test.circle.p2s = function(p1 = c(1,8), p2=c(5,5), slope = 1/12, dx=c(-5, 5),
 		col = 1, col.line = "#32D048", add = FALSE) {
