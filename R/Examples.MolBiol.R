ab0be109d8b6bd7e5e547c0e70d8ff721a34bf2e
discoleo <leo.mada@syonic.eu>
2026-03-17 22:37:17 +0200

    Gel: Loaded trays;

--- R/Examples.MolBiol.R
+++ R/Examples.MolBiol.R
@@ -4,9 +4,9 @@
 
 ### Empty Gel
 #' @export
-example.gel = function(h = 8, h.well = 1.5, x = c(0, 8), y = c(0,0),
-		fill = "#C2C2C2") {
-	tmp = gel(x, y, h=h, h.well = h.well, fill = fill)
+example.gel = function(h = 8, h.well = 1.5, n = 8, x = c(0, 8), y = c(0,0),
+		fill = c("#C2C2C2", "#3224E8")) {
+	tmp = gel(x, y, n=n, h=h, h.well = h.well, fill = fill)
 	plot.base(axt = NULL);
 	lines(tmp);
 	invisible(tmp);
@@ -15,4 +15,5 @@ example.gel = function(h = 8, h.well = 1.5, x = c(0, 8), y = c(0,0),
 # Slightly Skewed Gel:
 # - Student was not careful enough when placing the gel
 #   in the imaging device;
-# example.gel(x = c(0, 8), y = c(0, 1))
+# - Also forgot to run the electrophoresis;
+# example.gel(x = c(0, 8), y = c(0, 1), n = c(8, 6))
--- R/Graphic.MolBiol.Techniques.R
+++ R/Graphic.MolBiol.Techniques.R
@@ -16,9 +16,16 @@
 
 #' @export
 gel = function(x, y, n = 8, h = 8, h.well = 1.5, d = 1/4,
-		col = c("#B2B2B2", "#3224E8"), fill = "#C2C2C2") {
+		col = c("#B2B2B2"), fill = c("#C2C2C2", "#3224E8"),
+		tray.fill = 0.75) {
 	# Gel Boundaries:
 	xy = as.rect.fromLine(x, y, h=h);
+	if(length(n) == 0) {
+		class(xy) = c("polygon", class(xy));
+		xy = list(Gel = xy);
+		return(as.bioshape(xy));
+	}
+	nn = n; n = n[1];
 	# Bottom of wells:
 	tb = h.well / dist.xy(x, y); tbi = 1 - tb;
 	x1 = tb * x[1] + tbi * xy$x[4];
@@ -36,9 +43,25 @@ gel = function(x, y, n = 8, h = 8, h.well = 1.5, d = 1/4,
 	yy = rbind(xyT$y[id], xyT$y[id+1], xyB$y[id+1], xyB$y[id+2]);
 	xx = c(x[1], as.vector(xx), xyT$x[nE], x[2]);
 	yy = c(y[1], as.vector(yy), xyT$y[nE], y[2]);
-	res = list(x = xx, y = yy, col = col[1], fill = fill);
+	res = list(x = xx, y = yy, col = col[1], fill = fill[1]);
 	class(res) = c("polygon", "list");
 	res = list(Gel = res);
+	if(length(nn) == 1) return(as.bioshape(res));
+	### Loaded Gel:
+	tf = tray.fill; tf1 = 1 - tf;
+	xyTF   = list();
+	xyTF$x = tf * xyT$x + tf1 * xyB$x;
+	xyTF$y = tf * xyT$y + tf1 * xyB$y;
+	trays = lapply(seq(min(nn)), function(id) {
+		id2 = 2*id;
+		xi = c(xyTF$x[c(id2, id2 + 1)], xyB$x[c(id2 + 1, id2)]);
+		yi = c(xyTF$y[c(id2, id2 + 1)], xyB$y[c(id2 + 1, id2)]);
+		tmp = list(x = xi, y = yi, col = fill[2], fill = fill[2]);
+		class(tmp) = c("polygon", "list");
+		return(tmp);
+	})
+	trays = as.bioshape(trays);
+	res$Trays = trays;
 	return(as.bioshape(res));
 }
 
